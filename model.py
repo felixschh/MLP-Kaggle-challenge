230864b9736c81c62ca36a2c10685bff96b81714
Varun Kota <vkr.k8055@gmail.com>
2022-04-08 19:47:33 +0200

    updated

--- model.py
+++ model.py
@@ -4,50 +4,45 @@ from torch import nn
 # from torchsummary import summary
 
 class Classify(nn.Module):
-    def __init__(self, input):
+    def __init__(self):
         super(Classify, self).__init__()
-        self.input_layer = nn.Linear(input, 400)  
-        self.hidden1 = nn.Linear(400, 200) 
-        self.hidden2 = nn.Linear(200, 100) 
-        self.hidden3 = nn.Linear(100, 75)
-        self.hidden4 = nn.Linear(75, 50)
-        self.output = nn.Linear(50, 10) 
+        # self.input_layer = nn.Linear(784, 600)  
+        # self.hidden1 = nn.Linear(600, 400) 
+        # self.hidden2 = nn.Linear(400, 200) 
+        # self.hidden3 = nn.Linear(200, 100)
+        # #self.hidden4 = nn.Linear(64, 10)
+        # self.output = nn.Linear(100, 10) 
 
     # Batch normalisation n Dropout    
-        # self.fc1 = nn.Linear(input, 400)
-        # self.bn1 = nn.BatchNorm1d(400) # Batcn Norm for the first layer
-        # self.fc2 = nn.Linear(400, 200)
-        # self.bn2 = nn.BatchNorm1d(200) # Batcn Norm for the secon layer
-        # self.fc3 = nn.Linear(200, 100)
-        # self.bn3 = nn.BatchNorm1d(100)  # Batcn Norm for the third layer
-        # self.fc4 = nn.Linear(100, 75)
-        # self.bn4 = nn.BatchNorm1d(75)
-        # self.fc5 = nn.Linear(75, 50)
-        # self.bn5 = nn.BatchNorm1d(50)
-        # self.output = nn.Linear(50, 10)
-        # self.do  = nn.Dropout(0.2, inplace=True)
+        self.fc1 = nn.Linear(784, 600)
+        self.bn1 = nn.BatchNorm1d(600) 
+        self.fc2 = nn.Linear(600, 400)
+        self.bn2 = nn.BatchNorm1d(400) 
+        self.fc3 = nn.Linear(400, 200)
+        self.bn3 = nn.BatchNorm1d(200)  
+        self.fc4 = nn.Linear(200, 100)
+        self.bn4 = nn.BatchNorm1d(100)
+        self.output = nn.Linear(100, 10)
+        self.do  = nn.Dropout(0.2, inplace=True)
 
     
     def forward(self, x):
-        first_layer = self.input_layer(x)
-        act1 = F.relu(first_layer)
-        second_layer = self.hidden1(act1)
-        act2 = F.relu(second_layer)
-        third_layer = self.hidden2(act2)
-        act3 = F.relu(third_layer)
-        fourth_layer = self.hidden3(act3)
-        act4 = F.relu(fourth_layer)
-        third_layer = self.hidden4(act4)
-        act5 = F.relu(third_layer)
-        out_layer = self.output(act5)
-        x = F.softmax(out_layer, dim=1)
-        return out_layer
+        # first_layer = self.input_layer(x)
+        # act1 = F.relu(first_layer)
+        # second_layer = self.hidden1(act1)
+        # act2 = F.relu(second_layer)
+        # third_layer = self.hidden2(act2)
+        # act3 = F.relu(third_layer)
+        # fourth_layer = self.hidden3(act3)
+        # act4 = F.relu(fourth_layer)
+        # out_layer = self.output(act4)
+        # #x = F.softmax(out_layer, dim=1)
+        # return out_layer
 
-        # x = F.relu(self.bn1(self.do(self.fc1(x))))
-        # x = F.relu(self.bn2(self.do(self.fc2(x))))
-        # x = F.relu(self.bn3(self.do(self.fc3(x))))
-        # x = F.relu(self.bn4(self.do(self.fc4(x))))
-        # x = F.relu(self.bn5(self.do(self.fc5(x))))
-        # x = F.softmax(x, dim=1)
-        # return self.output(x)
+        x = F.relu(self.bn1(self.do(self.fc1(x))))
+        x = F.relu(self.bn2(self.do(self.fc2(x))))
+        x = F.relu(self.bn3(self.do(self.fc3(x))))
+        x = F.relu(self.bn4(self.do(self.fc4(x))))
+        x = F.softmax(x, dim=1)
+        return self.output(x)
 
--- train.py
+++ train.py
@@ -15,20 +15,21 @@ from model import Classify
 
 trainset, testset, trainloader, testloader = dh.get_data()
 
-model = Classify(784)
+model = Classify()
 
-criterion =  nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001)
+criterion =  nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=1e-3)
 
-epochs = 30
+epochs = 50
 train = []
 test = []
 print_every = 40
+running_loss_train = 0
+running_loss_test = 0
 starting_time = time.time()
 
 for epoch in range(epochs):
-    running_loss_train = 0
-    running_loss_test = 0
+    
 
     if epoch%10 == 0:
         torch.save(model.state_dict(), f'checkpoint_{epoch}.pth')
@@ -43,38 +44,39 @@ for epoch in range(epochs):
 
         logits = model.forward(images)
 
-        #prediction = F.log_softmax(logits, dim=1)#model.forward(images)
+        pred = F.log_softmax(logits, dim=1)#model.forward(images)
 
-        train_loss = criterion(logits, labels)
+        train_loss = criterion(pred, labels)
 
         train_loss.backward()
 
         optimizer.step()
 
-        model.eval()
-        with torch.no_grad():
-
-            for i, (images, labels) in enumerate(iter(testloader)):
-                images.resize_(images.size()[0], 784)
-
-                optimizer.zero_grad()
-
-                logits = model.forward(images)
-
-                test_loss = criterion(logits, labels)
-
-                test_loss.backward()
-
-                optimizer.step()
-
-                model.train()
-
-                running_loss_test += test_loss.item() 
-        running_loss_train += train_loss.item()
+        running_loss_train += train_loss.item() 
 
         if i%print_every ==0:
-                print(f"\tIteration: {i}\t Loss: {running_loss/print_every:.4f}")
-                running_loss=0
+            print(f"\tIteration: {i}\t TrainLoss: {running_loss_train/print_every:.4f} ")
+            running_loss_train=0
+
+    model.eval()
+    with torch.no_grad():
+
+        for i, (images, labels) in enumerate(iter(testloader)):
+            images.resize_(images.size()[0], 784)
+
+            logits = model.forward(images)
+
+            pred = F.log_softmax(logits, dim=1)
+
+            test_loss = criterion(pred, labels)
+
+            running_loss_test += test_loss.item() 
+
+            if i%print_every ==0:
+                print(f"\tIteration: {i} \t TestLoss: {running_loss_test/print_every:.4f}")
+                running_loss_test=0
+
+    model.train()
 
     train.append((running_loss_train/len(trainloader)))
     test.append((running_loss_test/len(testloader)))
@@ -90,4 +92,3 @@ torch.save(model.state_dict(), 'checkpoint.pth')
 plt.plot()
 plt.legend()
 plt.show()
-
